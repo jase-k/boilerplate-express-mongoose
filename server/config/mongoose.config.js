d533facf389699da852ed0d22bf60949dc49b86b
Jase Kraft <jase@perfectmypodcast.com>
2021-11-03 12:42:12 -0500

    comment out db name

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbNAME = "productManager_Schema"
+const dbNAME = // "productManager_Schema"
 
 mongoose.connect("mongodb://localhost/"+dbNAME, {
 	useNewUrlParser: true,
